6c4e3766eec28e7088ee6d26d7005e6a599db4a7
Ivar Thorson <thorsoni@ohsu.edu>
2013-02-05 14:24:30 -0800

    Sigmoidal now has an absolute value on its amplitude, so only rightward increasing sigmoids should appear. I hope this isn't a problem.

--- analysis/analysis_batch_240.sh
+++ analysis/analysis_batch_240.sh
@@ -2,7 +2,9 @@
 
 # A simple script test to launch four instances of matlab
 
-xterm -fg white -bg black -geometry 80x24+0+0     -e matlab -nodesktop -r "analysis_batch_240(1,4)" &
-xterm -fg white -bg black -geometry 80x24+600+0   -e matlab -nodesktop -r "analysis_batch_240(2,4)" &
-xterm -fg white -bg black -geometry 80x24+0+400   -e matlab -nodesktop -r "analysis_batch_240(3,4)" &
-xterm -fg white -bg black -geometry 80x24+600+400 -e matlab -nodesktop -r "analysis_batch_240(4,4)" &
+xterm -fg white -bg black -geometry 80x24+0+0     -e matlab -nodesktop -r "analysis_batch_240(1,6)" &
+xterm -fg white -bg black -geometry 80x24+600+0   -e matlab -nodesktop -r "analysis_batch_240(2,6)" &
+xterm -fg white -bg black -geometry 80x24+0+400   -e matlab -nodesktop -r "analysis_batch_240(3,6)" &
+xterm -fg white -bg black -geometry 80x24+600+400 -e matlab -nodesktop -r "analysis_batch_240(4,6)" &
+xterm -fg white -bg black -geometry 80x24+0+800   -e matlab -nodesktop -r "analysis_batch_240(5,6)" &
+xterm -fg white -bg black -geometry 80x24+600+800 -e matlab -nodesktop -r "analysis_batch_240(6,6)" &
