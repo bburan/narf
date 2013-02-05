11fbcbf432a48cd0f231c0d7704b6f05b1aa858b
Ivar Thorson <thorsoni@ohsu.edu>
2013-02-05 13:51:31 -0800

    Corrected workdir path for shell script.

--- analysis/analysis_batch_240.sh
+++ analysis/analysis_batch_240.sh
@@ -2,7 +2,7 @@
 
 # A simple script test to launch four instances of matlab
 
-konsole -e matlab -nodesktop -r "analysis_batch_240(1,4)" &
-konsole -e matlab -nodesktop -r "analysis_batch_240(2,4)" &
-konsole -e matlab -nodesktop -r "analysis_batch_240(3,4)" &
-konsole -e matlab -nodesktop -r "analysis_batch_240(4,4)" &
+konsole --workdir ./ -e matlab -nodesktop -r "analysis_batch_240(1,4)" &
+konsole --workdir ./ -e matlab -nodesktop -r "analysis_batch_240(2,4)" &
+konsole --workdir ./ -e matlab -nodesktop -r "analysis_batch_240(3,4)" &
+konsole --workdir ./ -e matlab -nodesktop -r "analysis_batch_240(4,4)" &
