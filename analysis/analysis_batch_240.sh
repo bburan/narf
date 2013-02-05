5e4fd0e5b7824f5c28ee14fb14010b7533edf9f6
Ivar Thorson <thorsoni@ohsu.edu>
2013-02-05 14:08:30 -0800

    Changing from Konsole to xterm, which is more standardized and actually lets you use -geometry flags like X has allowed for the last 20 years.

--- analysis/analysis_batch_240.sh
+++ analysis/analysis_batch_240.sh
@@ -2,7 +2,7 @@
 
 # A simple script test to launch four instances of matlab
 
-konsole --workdir ./ -e matlab -nodesktop -r "analysis_batch_240(1,4)" &
-konsole --workdir ./ -e matlab -nodesktop -r "analysis_batch_240(2,4)" &
-konsole --workdir ./ -e matlab -nodesktop -r "analysis_batch_240(3,4)" &
-konsole --workdir ./ -e matlab -nodesktop -r "analysis_batch_240(4,4)" &
+xterm -fg white -bg black -geometry 80x24+0+0     -e matlab -nodesktop -r "analysis_batch_240(1,4)" &
+xterm -fg white -bg black -geometry 80x24+600+0   -e matlab -nodesktop -r "analysis_batch_240(2,4)" &
+xterm -fg white -bg black -geometry 80x24+0+400   -e matlab -nodesktop -r "analysis_batch_240(3,4)" &
+xterm -fg white -bg black -geometry 80x24+600+400 -e matlab -nodesktop -r "analysis_batch_240(4,4)" &
