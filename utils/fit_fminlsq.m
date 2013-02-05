e6bf7b46a9e24c8e70b161c777bdd94e9fdc75b4
Ivar Thorson <thorsoni@ohsu.edu>
2013-02-05 15:32:22 -0800

    Found a 'fit_objective()' instance that I forgot to change to the new name, 'fit_fminsearch', which is more informative.

--- utils/fit_fminlsq.m
+++ utils/fit_fminlsq.m
@@ -1,6 +1,6 @@
 function termcond = fit_fminlsq()
 % TODO: What about non-default field names for the optimization?
 % This function needs arguments!
-    fit_objective();
+    fit_fminsearch();
     termcond = fit_lsq();
 end
