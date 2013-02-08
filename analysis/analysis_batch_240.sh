9d17756428892af667efb7b4f5720ca015dd8b38
Ivar Thorson <thorsoni@ohsu.edu>
2013-02-08 08:56:32 -0800

    Matlab's behavior wasn't as documented online, so now I'm using environment variables (MATLABPATH) to export before launching the xterms.

--- analysis/analysis_batch_240.sh
+++ analysis/analysis_batch_240.sh
@@ -4,9 +4,12 @@
 THISPATH=`pwd`/..
 BAPHYPATH=/home/ivar/matlab/baphy
 
-xterm -fg white -bg black -geometry 80x24+0+0     -e matlab $BAPHYPATH $THISPATH -nodesktop -r "analysis_batch_240(1,6)" &
-xterm -fg white -bg black -geometry 80x24+600+0   -e matlab $BAPHYPATH $THISPATH -nodesktop -r "analysis_batch_240(2,6)" &
-xterm -fg white -bg black -geometry 80x24+0+400   -e matlab $BAPHYPATH $THISPATH -nodesktop -r "analysis_batch_240(3,6)" &
-xterm -fg white -bg black -geometry 80x24+600+400 -e matlab $BAPHYPATH $THISPATH -nodesktop -r "analysis_batch_240(4,6)" &
-xterm -fg white -bg black -geometry 80x24+0+800   -e matlab $BAPHYPATH $THISPATH -nodesktop -r "analysis_batch_240(5,6)" &
-xterm -fg white -bg black -geometry 80x24+600+800 -e matlab $BAPHYPATH $THISPATH -nodesktop -r "analysis_batch_240(6,6)" &
+MATLABPATH=$THISPATH:$BAPHYPATH
+export MATLABPATH
+
+xterm -fg white -bg black -geometry 80x24+0+0     -e matlab -nodesktop -r "analysis_batch_240(1,6)" &
+xterm -fg white -bg black -geometry 80x24+600+0   -e matlab -nodesktop -r "analysis_batch_240(2,6)" &
+xterm -fg white -bg black -geometry 80x24+0+400   -e matlab -nodesktop -r "analysis_batch_240(3,6)" &
+xterm -fg white -bg black -geometry 80x24+600+400 -e matlab -nodesktop -r "analysis_batch_240(4,6)" &
+xterm -fg white -bg black -geometry 80x24+0+800   -e matlab -nodesktop -r "analysis_batch_240(5,6)" &
+xterm -fg white -bg black -geometry 80x24+600+800 -e matlab -nodesktop -r "analysis_batch_240(6,6)" &
