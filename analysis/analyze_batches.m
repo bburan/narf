cd5e8727528fde8c818ec6468e41ab92e1dcf42a
Ivar Thorson <thorsoni@ohsu.edu>
2013-02-25 11:15:16 -0800

    Special commit for running only gmm4, will run on hyrax.

--- analysis/analyze_batches.m
+++ analysis/analyze_batches.m
@@ -34,7 +34,8 @@ for bi = 1:length(batches)
     mm{1} = module_groups('env100');
     mm{2} = module_groups('log2b');
     mm{3} = module_groups('firn');
-    mm{4} = module_groups('npfnl', 'npfnl3', 'npnl', 'senl', 'senl3'); %gmm4
+    %mm{4} = module_groups('npfnl', 'npfnl3', 'npnl', 'senl', 'senl3'); 
+    mm{4} = module_groups('gmm4'); 
     mm{5} = module_groups('fmin', 'boost', 'sb');
     mm{6} = module_groups('mse','mses2','mses3','mses4','mses5','mses6');
     
