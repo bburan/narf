bc8b34321db3c1f72b9e0e2482adf315b5c1229d
Ivar Thorson <thorsoni@ohsu.edu>
2013-02-25 12:27:46 -0800

    Meaningless commit for workernarf. Currently work is partitioned across 4 machines.

--- analysis/analyze_batches.m
+++ analysis/analyze_batches.m
@@ -34,8 +34,10 @@ for bi = 1:length(batches)
     mm{1} = module_groups('env100');
     mm{2} = module_groups('log2b');
     mm{3} = module_groups('firn');
-    %mm{4} = module_groups('npfnl', 'npfnl3', 'npnl', 'senl', 'senl3'); 
-    mm{4} = module_groups('gmm4'); 
+    %mm{4} = module_groups('npfnl', 'npfnl3'); % On microbat
+    %mm{4} = module_groups('gmm4');  % On hyrax
+    %mm{4} = module_groups('senl', 'senl3'); % On bobcat
+    mm{4} = module_groups('npnl');  %Local
     mm{5} = module_groups('fmin', 'boost', 'sb');
     mm{6} = module_groups('mse','mses2','mses3','mses4','mses5','mses6');
     
