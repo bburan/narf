37fe72e3be34b05ffffe85015a66baca38282822
Ivar Thorson <thorsoni@ohsu.edu>
2013-02-27 10:09:06 -0800

    Commiting a new file for batch 240 analysis documentation.

--- analysis/analyze_batches.m
+++ analysis/analyze_batches.m
@@ -43,7 +43,7 @@ for bi = 1:length(batches)
     %else
     %    mm{4} = module_groups('npnl');  % On badger or localhost
     %end
-    % mm{4} = module_groups('npnl', 'npfnl', 'npfnl3', 'senl', 'senl3', 'gmm4');
+    mm{4} = module_groups('npnl', 'npfnl', 'npfnl3', 'senl', 'senl3', 'gmm4');
     mm{5} = module_groups('fmin', 'boost', 'sb', 'fminlsq');
     mm{6} = module_groups('mse', 'mses2','mses3','mses4','mses5','mses6');
     
@@ -53,31 +53,31 @@ for bi = 1:length(batches)
         if mod(ii + instance_num, total_instances) ~= 0
             continue
         end
-        fit_models(mm, cells{ii}.cellid, ...
-             cells{ii}.training_set, ...
-             cells{ii}.test_set);
+        %fit_models(mm, cells{ii}.cellid, ...
+        %     cells{ii}.training_set, ...
+        %     cells{ii}.test_set);
         
         % Technically, fit_models already built a perfect cache, even if interrupted,
         % but rebuilding it can give us a little peace of mind so let's do it.
-        %summarize_cellid(cells{ii}.cellid, true);
+        summarize_cellid(cells{ii}.cellid, true);
         
         % Otherwise, load the model summaries for this cell from the cache
-        %sf = [NARF_SAVED_ANALYSIS_PATH filesep cells{ii}.cellid '_summary.mat'];
-        %summaries = load_summaries({sf});
+        sf = [NARF_SAVED_ANALYSIS_PATH filesep cells{ii}.cellid '_summary.mat'];
+        summaries = load_summaries({sf});
         
         % Limit the summaries to only those model combinations found in mm
-        %summaries = only_named_summaries(summaries, modelnames);
+        summaries = only_named_summaries(summaries, modelnames);
         
         % Generate PNGs showing the best models, tokens for each cellid
-        %plot_cellid_summary(cells{ii}.cellid, summaries, true, analysis_prefix);
+        plot_cellid_summary(cells{ii}.cellid, summaries, true, analysis_prefix);
         
     end
     
     % Finally, display and save heat maps of test and training set performance
-    %summary_files = cellfun(@(x) [NARF_SAVED_ANALYSIS_PATH filesep x.cellid '_summary.mat'], ...
-%        cells, 'UniformOutput', false);
-%    summaries = load_summaries(summary_files);
-%    summaries = only_named_summaries(summaries, modelnames);
-%    
-%    plot_summaries(summaries, analysis_prefix, true);
+    summary_files = cellfun(@(x) [NARF_SAVED_ANALYSIS_PATH filesep x.cellid '_summary.mat'], ...
+        cells, 'UniformOutput', false);
+    summaries = load_summaries(summary_files);
+    summaries = only_named_summaries(summaries, modelnames);
+    
+    plot_summaries(summaries, analysis_prefix, true);
 end
