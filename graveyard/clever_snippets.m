96934f9f5a29cc02309bc2464d24a1b99c3dd36c
Ivar Thorson <thorsoni@ohsu.edu>
2013-03-27 15:47:06 -0700

    Moving outdated plot commands to the graveyard.

--- graveyard/clever_snippets.m
+++ graveyard/clever_snippets.m
@@ -1,3 +1,31 @@
+% % ----------------------
+% For plotting a test/train scatter plot
+% % % Also, show the test/train curves
+% fh_tt = figure;
+% plot(1:length(filenames), score(:,1), ...
+%      1:length(filenames), score(:,3));
+% xticks(1:length(filenames));
+% 
+% % label the x axis with filenames at a 90 degree angle
+% lab = char(sorted_filenames);
+% hx = get(gca,'XLabel');
+% set(hx, 'Units', 'data');
+% pos = get(hx, 'Position');
+% 
+% for ii = 1:size(lab,1)
+%     t(ii) = text(ii, pos(2), lab(ii,:), 'Interpreter', 'none');
+% end
+% 
+% P=get(gca,'Position');
+% set(t, 'Rotation', 90, 'HorizontalAlignment', 'right') 
+% set(gca, 'Position', [0.1300    0.4100    0.7750    0.5150])
+% 
+% legend('Test Score', 'Training Score', 'Location', 'NorthWest');
+% 
+% title(filenames{1}, 'Interpreter', 'none');
+
+% fh1 = fig;
+% fh2 = fh_tt;
 
         % ---------------------------
 % FIR_FILTER.m
