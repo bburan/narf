91630726ec7c33dab27cb38afe983534185031d2
Ivar Thorson <thorsoni@ohsu.edu>
2013-04-01 15:52:05 -0700

    Added documentation for pack_fittables.

--- utils/pack_fittables.m
+++ utils/pack_fittables.m
@@ -1,10 +1,19 @@
 function w = pack_fittables(stack)
+% w = pack_fittables(stack)
+%
+% Packs into a single vector w all the elements of each field of each
+% module that has a corresponding field "fit_fields" containing field
+% names. Got it? ;-)
+%
+% Used by fitters to pack and unpack checked values on the stack. 
+%
+% Only works for matrix, vector, and numeric types. 
 
 w = [];
 for ii = 1:length(stack)
     if isfield(stack{ii}, 'fit_fields')
         for jj = 1:length(stack{ii}.fit_fields),
-            p=stack{ii}.fit_fields{jj};
+            p = stack{ii}.fit_fields{jj};
             w = cat(1, w, reshape(stack{ii}.(p), numel(stack{ii}.(p)), 1));
         end
     end
--- utils/pickcolor.m
+++ utils/pickcolor.m
@@ -1,5 +1,16 @@
 function c = pickcolor(n)
-m = mod(n,7);
+% c = pickcolor(n)
+%
+% Returns a color and linestyle for a particular index. 
+% Useful when 'hold on' is set and you need to plot different lines with
+% repeated calls to PLOT() but want them to be the proper colors.
+%
+% Essentially, this function is a very clumsy way of accomplishing this:
+%
+% set(gca, 'ColorOrder', [0 0 1; 0 1 0; 1 0 0; 0 1 1; 1 0 1; 1 1 0;], ...
+%         'LineStyleOrder',{'-','--',':'},'NextPlot','ReplaceChildren');
+
+m = mod(n,19);
 switch m
     case 0 
         c='k-';
@@ -15,4 +26,28 @@ switch m
         c='m-';
     case 6
         c='y-';
+    case 7
+        c='b--';
+    case 8 
+        c='g--';
+    case 9 
+        c='r--';
+    case 10 
+        c='c--';
+    case 11
+        c='m--';
+    case 12
+        c='y--';
+    case 13
+        c='b:';
+    case 14
+        c='g:';
+    case 15
+        c='r:';
+    case 16 
+        c='c:';
+    case 17
+        c='m:';
+    case 18
+        c='y:';      
 end
