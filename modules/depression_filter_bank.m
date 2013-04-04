ab555a5736807faede9724f516544e1795f79ad1
Stephen David <stephen.v.david@gmail.com>
2013-04-04 13:28:43 -0700

    Added pedstal to depression_bank

--- modules/depression_filter_bank.m
+++ modules/depression_filter_bank.m
@@ -8,13 +8,14 @@ m.name = 'depression_filter_bank';
 m.fn = @do_depression_filter;
 m.pretty_name = 'Depression Filter Bank';
 m.editable_fields = {'num_channels', 'strength', 'tau', ...
-                     'input', 'time', 'output' };
+                     'pedestal', 'input', 'time', 'output' };
 m.isready_pred = @isready_always;
 
 % Module fields that are specific to THIS MODULE
 m.num_channels = 2;
 m.strength = [0 0.2];  % as fraction of stimulus max magnitude
 m.tau = [0 100];  % in ms
+m.pedestal = 0;  % as fraction of stimulus max magnitude
 m.raw_stim_freq = 100000;
 m.input = 'stim';
 m.time = 'stim_time';
@@ -36,6 +37,9 @@ m.plot_fns{2}.pretty_name = 'Filtered Stimulus vs Time';
 function x = do_depression_filter(stack, xxx)
     mdl = stack{end};
     x = xxx{end};
+    if ~isfield(mdl,'pedestal'),
+        mdl.pedestal=0;
+    end
     
     baphy_mod = find_module(stack, 'load_stim_resps_from_baphy');
     %stimmax=x.stimminmax(2,:);
@@ -59,7 +63,8 @@ function x = do_depression_filter(stack, xxx)
         [T, S, N] = size(x.dat.(sf).(mdl.input));
         ret = zeros(T, S, N*mdl.num_channels);
         for s = 1:S
-            stim_in=squeeze(x.dat.(sf).(mdl.input)(:,s,:))';
+            stim_in=squeeze(x.dat.(sf).(mdl.input)(:,s,:))' + ...
+                    repmat(mdl.pedestal./stimmax(:),[1,T]);
             depresp=depression_bank(stim_in,(1./stimmax(:))*mdl.strength,...
                                     mdl.tau.*baphy_mod.raw_stim_fs./1000,1)';
             depresp=permute(depresp,[1 3 2]);
