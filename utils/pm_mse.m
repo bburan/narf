27fed6a918ad43f2a00009477f76b7e49c957c36
Ivar Thorson <thorsoni@ohsu.edu>
2013-05-02 12:14:13 -0700

    Small correction to pm_mse

--- utils/pm_mse.m
+++ utils/pm_mse.m
@@ -3,12 +3,12 @@ function score = pm_mse()
 %
 % Performance metric: MSE with optional sparseness penalty
 
-global XXX META;
+global XXX META STACK;
 
 if ~isfield(META, 'sparsity_weight')
     score = XXX{end}.score_train_mse; 
 else
-    [fir_mods, fir_idxs] = find_modules('fir_filter');
+    [fir_mods, fir_idxs] = find_modules(STACK, 'fir_filter');
     sparsities = zeros(size(fir_idxs));   
     for ii=1:length(fir_idxs)
         sparsities(ii) = sparsity_metric(fir_mods{ii}.coefs);
