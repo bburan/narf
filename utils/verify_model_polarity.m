517d30792e0263b267fa92c2eef232a70f97f1a3
Ivar Thorson <thorsoni@ohsu.edu>
2013-05-06 13:37:04 -0700

    About 15 trials later, I _think_ that this works more reliably for verify_model_polarity.

--- utils/verify_model_polarity.m
+++ utils/verify_model_polarity.m
@@ -27,34 +27,69 @@ for kk = 1:length(firmod_idxs)
         V1 = cat(1, V1, x.dat.(sf).(firmod.output)(:));
         V2 = cat(1, V2, x.dat.(sf).respavg(:));
     end
-    D = [V1(:) V2(:)]; 
+   
+    [~, idxs, ~] = unique(V1, 'first'); % Remove duplicates
+    D = [V1(idxs) V2(idxs)]; 
     D = excise(D);
     D = sortrows(D);
-    
-    % OLD way wasn't sufficiently reliable: just linear isn't enough?
-    % P = polyfit(D(:,1),D(:,2), 1);
-    %if (P(1) < 0)
-    %    for aa = 1:length(STACK{idx})
-    %        STACK{idx}{aa}.coefs = - STACK{idx}{aa}.coefs;
-    %    end
-    %end
-    
-    % New way: try to fit a sigmoid to the curve 
+    D = conv_fn(D, 1, @nanmean, ceil(size(D, 1)/100), 0);
     xs = D(:,1);
     ys = D(:,2);
-    phi_init1 = [mean(xs), var(xs), 0.5*(max(xs)-min(xs)), 0]; 
-    phi_init2 = [- mean(xs), var(xs), 0.5*(max(xs)-min(xs)), 0];    
-    opts = optimset('Display','off');
-    [phi_best1, rn1] = lsqcurvefit(@nl_sigmoid, ...
-                           phi_init1, xs, ys, [], [], opts);
-    [phi_best2, rn2] = lsqcurvefit(@nl_sigmoid, ...
-                           phi_init2, xs, ys, [], [], opts);
     
-    if (rn2 < rn1)
+%     % Maybe filtering will help remove outliers?
+% 	winsize = ceil(length(ys) / 4);
+%     if mod(winsize, 2) == 1
+%         winsize = winsize + 1;
+%     end
+%     gf = gausswin(winsize);
+%     gf = gf / sum(gf);
+%     %x = filter(gf, 1, xs);
+%     y = filter(gf, 1, ys);    
+    idx10 = ceil(length(ys) / 20);
+    y1 = mean(ys(1:idx10));
+    y2 = mean(ys(end-idx10:end));
+    if (y1 > y2)
+        fprintf('verify_model_polarity.m: Detected negative slope. Flipping.\n');
         for aa = 1:length(STACK{idx})
             STACK{idx}{aa}.coefs = - STACK{idx}{aa}.coefs;
         end
-    end   
+    end
+    
+    % Possible 4th attempt: Find the index of the peak point
+    % If it is to the 'left' of the center of the range, flip?
+    
+    % Debug crap
+    figure; plot(xs, ys, 'b.');
+    figure; imagesc(STACK{idx}{1}.coefs);
+    ca = caxis;
+    lim = max(abs(ca));
+    caxis([-lim, +lim]);
+    keyboard
+    
+    % OLD way wasn't sufficiently reliable: just linear isn't enough?
+    % P = polyfit(D(:,1),D(:,2), 1);
+    % if (P(1) < 0)
+    %     for aa = 1:length(STACK{idx})
+    %         STACK{idx}{aa}.coefs = - STACK{idx}{aa}.coefs;
+    %     end
+    % end
+    
+    % Another way: try to fit a sigmoid to the curve 
+%     xs = D(:,1);
+%     ys = D(:,2);
+%     phi_init1 = [mean(xs), var(xs), 0.5*(max(xs)-min(xs)), 0]; 
+%     phi_init2 = [- mean(xs), var(xs), 0.5*(max(xs)-min(xs)), 0];    
+%     opts = optimset('Display','off');
+%     [phi_best1, rn1] = lsqcurvefit(@nl_sigmoid, ...
+%                            phi_init1, xs, ys, [], [], opts);
+%     [phi_best2, rn2] = lsqcurvefit(@nl_sigmoid, ...
+%                            phi_init2, xs, ys, [], [], opts);
+%     if (rn2 < rn1)
+%         for aa = 1:length(STACK{idx})
+%             STACK{idx}{aa}.coefs = - STACK{idx}{aa}.coefs;
+%         end
+%     end   
+    
     
     calc_xxx(idx); 
     
