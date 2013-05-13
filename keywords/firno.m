15f8742fd21d12acd21b6372d055feebe15cf68c
Ivar Thorson <thorsoni@ohsu.edu>
2013-05-13 16:12:59 -0700

    Added a baseline to firno.

--- keywords/firno.m
+++ keywords/firno.m
@@ -5,4 +5,4 @@ global MODULES;
 append_module(MODULES.normalize_channels.mdl(struct('force_positive', true)));
 
 append_module(MODULES.fir_filter.mdl(struct('num_coefs', 12, ...
-                                            'fit_fields', {{'coefs'}})));
+                                            'fit_fields', {{'coefs', 'baseline'}})));
