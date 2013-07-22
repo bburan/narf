2f2cbd8c6142b2c6a01275d9e60241d0a8142a3a
Ivar Thorson <thorsoni@ohsu.edu>
2013-07-22 15:20:58 -0700

    Adding env100norm to test if normalization would help compressors.

---
+++ keywords/env100norm.m
@@ -0,0 +1,13 @@
+function env100norm()
+
+global MODULES;
+
+append_module(MODULES.load_stim_resps_from_baphy.mdl(...
+                           struct('raw_resp_fs', 100, ...
+                                  'raw_stim_fs', 100,...
+                                  'include_prestim', true, ...
+                                  'stimulus_format', 'envelope'))); 
+
+append_module(MODULES.normalize_channels);
+
+                              
