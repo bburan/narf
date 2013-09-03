9cb6f0dc94deccba039880e60e9b99cc4e67f481
Ivar Thorson <thorsoni@ohsu.edu>
2013-09-03 16:32:32 -0700

    Correction to poly2 poly3 that allows them to be used as compressors.

--- keywords/poly2.m
+++ keywords/poly2.m
@@ -4,9 +4,10 @@ global MODULES XXX;
 
 ff=XXX{end}.training_set{1};
 resp = XXX{end}.dat.(ff).respavg(:);
-stim = nanmean(XXX{end}.dat.(ff).stim(:));
+stim = XXX{end}.dat.(ff).stim(:);
 
-phi_init = polyfit(stim, resp, 2);
+% This next line is pretty hacky, since it only uses one channel of stim
+phi_init = polyfit(stim(1:length(resp)), resp, 2);
 append_module(MODULES.nonlinearity.mdl(struct('fit_fields', {{'phi'}}, ...
                                                   'phi', phi_init, ...
                                                   'nlfn', @polyval)));             
--- keywords/poly3.m
+++ keywords/poly3.m
@@ -4,9 +4,9 @@ global MODULES XXX;
 
 ff=XXX{end}.training_set{1};
 resp = XXX{end}.dat.(ff).respavg(:);
-stim = nanmean(XXX{end}.dat.(ff).stim(:));
+stim = XXX{end}.dat.(ff).stim(:);
 
-phi_init = polyfit(stim, resp, 3);
+phi_init = polyfit(stim(1:length(resp)), resp, 2);
 append_module(MODULES.nonlinearity.mdl(struct('fit_fields', {{'phi'}}, ...
                                                   'phi', phi_init, ...
                                                   'nlfn', @polyval)));
