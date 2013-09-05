3951716046f0ba21f74a56d7db5a9e1f898205b8
Stephen David <davids@ohsu.edu>
2013-09-05 15:51:08 -0700

    a couple fixes to some of the exotic core filters

--- keywords/dep1perchan.m
+++ keywords/dep1perchan.m
@@ -1,25 +1,28 @@
-function depfree1perchan()
+function dep1perchan()
 
 global MODULES;
-global XXX;
+global XXX STACK;
 
 append_module(MODULES.normalize_channels.mdl(struct('force_positive', true)));
 
 sf=XXX{end}.training_set{1};
 chan_count=size(XXX{end}.dat.(sf).stim,3);
 init_str=repmat(0.1,[1 chan_count]);
-init_tau=repmat(30,[1 chan_count]);
+init_tau=repmat(3,[1 chan_count]);
 
 append_module(MODULES.depression_filter_bank.mdl(...
                     struct('strength', init_str, ...
                            'tau',      init_tau, ...
-                           'tau_norm',  1000,...
+                           'tau_norm',  100,...
                            'per_channel', 1, ...
                            'fit_fields', {{'strength', 'tau'}})));
 
 append_module(MODULES.normalize_channels.mdl(struct('force_positive', true)));
 
+meanresp = nanmean(flatten_field(XXX{end}.dat,XXX{end}.training_set,'respavg'));
 append_module(MODULES.fir_filter.mdl(struct('num_coefs', 12, ...
+                            'baseline',meanresp,...
                             'fit_fields', {{'coefs','baseline'}})));
 
 fitSubstack();
+fitSubstack(length(STACK)-2,10^-2.5);
