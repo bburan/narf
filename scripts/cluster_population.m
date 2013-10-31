3341f06b3c70ed448521560e544c1c7a1d2a8ee6
Ivar Thorson <thorsoni@ohsu.edu>
2013-10-31 16:19:10 -0700

    Small corrections to cluster_population.m and some documentation changes.

--- scripts/cluster_population.m
+++ scripts/cluster_population.m
@@ -1,11 +1,24 @@
-function cluster_population(batch, cellids, modelnames)
+function cluster_population(batch, cellids, modelnames, name_statistic, name_noise)
+%cluster_population(batch, cellids, modelnames, name_statistic, name_noise)
+%
+% ARGUMENTS:
+%    name_statistic     Defaults to 'r_test'
+%    name_noise         Defaults to "r_floor'. Set to [] to disable
+%                       significance testing based on this value.
+%
+% ALGORITHM:
 % For each cellid
 %    1. Check all pairings of models (n^2 comparisons for n models)
 %    2. Record which pairings are "significantly" better
 %    3. Create a population PIE chart from those pairings.
 
-name_statistic = 'r_test';
-name_noise     = 'r_floor';
+if ~exist('name_statistic', 'var')
+    name_statistic = 'r_test';
+end
+if ~exist('name_noise', 'var')
+    name_noise     = 'r_floor';
+    %name_noise = [];
+end
 
 function [scores, noises] = scores_for_cellid(bat, cellid, models)
     scores = [];
@@ -22,7 +35,9 @@ function [scores, noises] = scores_for_cellid(bat, cellid, models)
             error('Too many results found!!? WTF!?\n');
         else
             scores(ii) = abs(results.(name_statistic));
-            noises(ii) = abs(results.(name_noise));
+            if ~isempty(name_noise)
+                noises(ii) = abs(results.(name_noise));
+            end
         end       
     end
 end
@@ -36,7 +51,9 @@ for ci = 1:N_cells
     fprintf('Computing scores for cellid: %s [%d/%d]\n', cellids{ci}, ci, N_cells); 
    	[ret1, ret2] = scores_for_cellid(batch, cellids{ci}, modelnames);
     scores(ci, :) = ret1(:);
-    noises(ci, :) = ret2(:);
+    if ~isempty(name_noise)
+        noises(ci, :) = ret2(:);
+    end
 end
 
 noises = mean(noises, 2); % Average noise floor across all models
@@ -91,10 +108,12 @@ for li = 1:length(uniqs)
 end
 
 % Make pie chart
-figure('Name', 'Population Clusters', 'NumberTitle', 'off');
+figure('Name', 'Population Clusters', 'NumberTitle', 'off', 'Position', [100, 100, 1000, 1000]);
 h = pie(frequency, thelabels);
-for ni = 1:N
+for ni = 1:length(frequency)
     set(h(2*ni), 'Interpreter', 'none');
 end
+ht = title(sprintf('Population Clustering by "%s" with noise floor "%s"\nValid for comparing 2 models...less accurate as you add more.\nFive percent of the %d neurons displayed will be misclassified', name_statistic, name_noise, length(cellids)));
+set(ht, 'Interpreter', 'none');
 
 end
