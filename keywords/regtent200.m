b738548034ec2ca197a145dab24bcffeb5965ece
Ivar Thorson <thorsoni@ohsu.edu>
2013-11-19 16:04:51 -0800

    Adding two tent-regularization keywords for 200 and 400Hz.

---
+++ keywords/regtent200.m
@@ -0,0 +1,5 @@
+function regtent200()
+
+global MODULES;
+
+append_module(MODULES.smooth_respavg.mdl(struct('window', [1 4 1])));
---
+++ keywords/regtent400.m
@@ -0,0 +1,5 @@
+function regtent400()
+
+global MODULES;
+
+append_module(MODULES.smooth_respavg.mdl(struct('window', [1 3 5 3 1])));
