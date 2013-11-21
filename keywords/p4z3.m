fd4cbc76765af607a58618cfad26a156853612b4
Ivar Thorson <thorsoni@ohsu.edu>
2013-11-21 08:54:33 -0800

    Adding two more pole/zero keywords.

---
+++ keywords/p4z3.m
@@ -0,0 +1,12 @@
+function p4z3 ()
+
+global MODULES;
+
+append_module(MODULES.normalize_channels.mdl(struct('force_positive', true)));
+
+append_module(MODULES.pole_zeros.mdl(...
+                struct('fit_fields', {{'poles', 'zeros', 'gains', 'delays', 'y_offset'}}, ...
+                       'n_poles', 4, ...
+                       'n_zeros', 3)));
+
+end
