d42b8617ff2bc801d2e11c4cb2a3e6601db92a5d
Ivar Thorson <thorsoni@ohsu.edu>
2013-12-10 11:46:34 -0800

    PZ Wavelet and Syn models commited while I work on 12ch gammatone.

--- modules/concatenate_channels.m
+++ modules/concatenate_channels.m
@@ -1,5 +1,5 @@
 function m = concatenate_channels(args)
-% Concatenates two signals together
+% Concatenates multiple signals together
 
 % Module fields that must ALWAYS be defined
 m = [];
@@ -7,12 +7,11 @@ m.mdl = @concatenate_channels;
 m.name = 'concatenate_channels';
 m.fn = @do_concatenate_channels;
 m.pretty_name = 'Concatenate Channels';
-m.editable_fields = {'input', 'time', 'output'};
+m.editable_fields = {'inputs', 'time', 'output'};
 m.isready_pred = @isready_always;
 
 % Module fields that are specific to THIS MODULE
-m.input1 = 'stim'; 
-m.input2 = 'stim2';
+m.inputs = {'stim1', 'stim2'}; 
 m.output = 'stim';
 
 % Optional fields
@@ -30,24 +29,33 @@ end
 
 function x = do_concatenate_channels(mdl, x, stack, xxx)    
 
-    for sf = fieldnames(x.dat)', sf=sf{1};           
-        [T1, S1, C1] = size(x.dat.(sf).(mdl.input1));
-        [T2, S2, C2] = size(x.dat.(sf).(mdl.input1));
-        
-        if (T1 ~= T2 || S1 ~= S2)
-            error('Concatenated channels must have the same length in time and stimulus dimensions!');
-        end
+    for sf = fieldnames(x.dat)', sf=sf{1}; 
 
-        out = zeros(T1,S1, C1+C2);
-        
-        for ii = 1:(C1+C2) 
-            if (ii <= C1)
-                out(:,:,ii) = x.dat.(sf).(mdl.input1)(:,:,ii);
-            else
-                out(:,:,ii) = x.dat.(sf).(mdl.input1)(:,:,ii-C1);
+        % Allocate space
+        Ts=[]; Ss=[]; Cs=[];        
+        for cc = 1:length(mdl.inputs)
+            [T, S, C] = size(x.dat.(sf).(mdl.inputs{cc}));            
+            Ts(end+1) = T;
+            Ss(end+1) = S;
+            Cs(end+1) = C;
+        end
+        if all(Ts(1) == Ts) && all(Ss(1) == Ss)
+            out = zeros(max(Ts),max(Ss), sum(Cs));
+        else
+            fprintf('Ts = %d', Ts);
+            fprintf('Ss = %d', Ss);            
+            error('Error in sizes of concatenated channels\n');
+        end
+    
+        % Concatenate all channels
+        c_idx = 1;
+        for cc = 1:length(mdl.inputs)
+            [T, S, C] = size(x.dat.(sf).(mdl.inputs{cc}));            
+            for ii = 1:C 
+                out(:,:,c_idx) = x.dat.(sf).(mdl.inputs{cc})(:,:,ii);
+                c_idx = c_idx + 1;
             end
         end
-        
         x.dat.(sf).(mdl.output) = out;
     end
 end
---
+++ modules/pz_wavelet.m
@@ -0,0 +1,149 @@
+function m = pz_wavelet(args)
+
+% Module fields that must ALWAYS be defined
+m = [];
+m.mdl = @pz_wavelet;
+m.name = 'pz_wavelet';
+m.fn = @do_pz_wavelet;
+m.pretty_name = 'PZ Wavelet';
+m.editable_fields = {'N_zeros', 'center_freq', 'Q_factor', 'N_order', ...
+                     'delayms', 'gain', 'y_offset', ...
+                     'input', 'time', 'output'};  
+m.isready_pred = @isready_always;
+
+% Module fields that are specific to THIS MODULE
+m.poles       = [];
+m.zeros       = [];
+m.N_zeros     = 0;
+m.center_freq = 200; % In decihertz
+m.Q_factor    = 3; % Actually the offset from sqrt(2)
+m.N_order     = 4; 
+m.delayms     = 5; % Input delays in ms
+m.gain        = 1;
+m.y_offset    = 0;
+
+m.input =  'stim';
+m.time =   'stim_time';
+m.output = 'stim';
+
+% Optional fields
+m.is_splittable = true;
+m.auto_plot = @do_plot_pz_impulse_response;
+m.plot_fns = {};
+m.plot_fns{1}.fn = @do_plot_single_default_output;
+m.plot_fns{1}.pretty_name = 'Output vs Time';
+m.plot_fns{2}.fn = @do_plot_pz_impulse_response;
+m.plot_fns{2}.pretty_name = 'Impulse Response';
+m.plot_fns{3}.fn = @do_plot_pz_step_response;
+m.plot_fns{3}.pretty_name = 'Step Response';
+m.plot_fns{4}.fn = @do_plot_pz_bodemag_plot;
+m.plot_fns{4}.pretty_name = 'Bode Mag. Plot';
+m.plot_fns{5}.fn = @do_plot_zplane;
+m.plot_fns{5}.pretty_name = 'ZPlane Plot';
+
+% Overwrite the default module fields with arguments 
+if nargin > 0
+    m = merge_structs(m, args);
+end
+
+% ------------------------------------------------------------------------
+% INSTANCE METHODS
+
+function sys = makesys(mdl)
+    CF = abs(mdl.center_freq) * 10;
+    Q  = 0.71 + abs(mdl.Q_factor);
+    N  = ceil(abs(mdl.N_order));
+    
+    % Calculate useful parameterizations
+    w_c = 2*pi*CF;    
+    theta = asin(1-(1/(2*Q^2)));
+    w_n = w_c / sqrt(1-(1/(2*Q^2))); % Natural frequency in rad/sec
+
+    % Create a set of 2nd order poles that will be repeated N times
+    b   = w_n * cos(theta/2 + pi/4);
+    w_r = w_n * sin(theta/2 + pi/4);
+    poles = [-b + 1i*w_r; -b - 1i*w_r];
+    
+    % Replicate the number of poles depending on the order of the filter
+    poles = repmat(poles, N, 1);    
+    
+    if (mdl.N_zeros == 0)
+        % Create the APGF to have a gain of 1 at DC
+        sys = zpk([], poles, 1);
+        sys = zpk([], poles, 1/dcgain(sys));
+    else
+        z = zeros(N, 1);
+        sys = zpk(z, poles, 1);
+        sys = zpk(z, poles, (1/cos(theta))/real(evalfr(sys, w_c*1j))); 
+    end
+
+    sys.InputDelay = abs(mdl.delayms) / 1000; % (milliseconds)
+end
+
+function x = do_pz_wavelet(mdl, x, stack, xxx)    
+    sys = makesys(mdl);    
+    for sf = fieldnames(x.dat)', sf=sf{1};        
+         [T, S, C] = size(x.dat.(sf).(mdl.input));         
+         tmp = zeros(T, S, 1);                                  
+                  
+         for s = 1:S            
+             t = x.dat.(sf).(mdl.time)(:,1);
+             u = squeeze(x.dat.(sf).(mdl.input)(:, s, :));
+             
+             % If there are NANs in the input, treat them like 0's when
+             % simulating with lsim, then NAN out the output later.
+             % This is kind of an ugly hack. 
+             u_nan = isnan(u);
+             u(u_nan) = 0;
+             u(isinf(u)) = 10^6;
+             warning off Control:analysis:LsimStartTime;
+             tmp(:,s) = lsim(sys, u, t);
+             
+             warning on Control:analysis:LsimStartTime;
+             nanidxs = any(u_nan,2);
+             tmp(nanidxs,s) = nan;
+                          
+         end
+         % Apply the post-filter function
+         x.dat.(sf).(mdl.output) = mdl.gain * abs(tmp) + mdl.y_offset;
+    end
+end
+
+function do_plot_pz_impulse_response(sel, stack, xxx)
+    mdls = stack{end};
+    xins = {xxx(1:end-1)};        
+    sys = makesys(mdls{1});
+    impulse(sys);   
+    do_xlabel('Time [s]');
+    do_ylabel('Impulse Response');
+end
+
+function do_plot_pz_step_response(sel, stack, xxx)
+    mdls = stack{end};
+    xins = {xxx(1:end-1)};    
+    sys = makesys(mdls{1});
+    step(sys);
+    do_xlabel('Time [s]');
+    do_ylabel('Step Response');    
+end
+
+function do_plot_pz_bodemag_plot(sel, stack, xxx)
+    mdls = stack{end};
+    xins = {xxx(1:end-1)};        
+    sys = makesys(mdls{1});
+    h = bodeplot(sys);
+    setoptions(h,'FreqUnits','Hz','PhaseVisible','off');
+    do_xlabel('Freq [Hz]');
+    do_ylabel('Freq Response');
+end
+
+function do_plot_zplane(sel, stack, xxx)
+    mdls = stack{end};
+    xins = {xxx(1:end-1)};        
+    sys = makesys(mdls{1});
+    zplane(sys.z, sys.p);
+    do_xlabel('Real Axis');
+    do_ylabel('Imaginary Axis');
+end
+
+end
