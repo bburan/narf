2170a3072b3428f67144067de5b49523fada0fb5
Ivar Thorson <thorsoni@ohsu.edu>
2014-03-11 14:28:53 -0700

    Making a variation on weight channels keywords that does a little fitting before continuing.

---
+++ keywords/firtemp.m
@@ -0,0 +1,14 @@
+function firtemp()
+% Fit a FIR filter temporarily
+global MODULES XXX;
+signal = 'stim';
+fns = fieldnames(XXX{end}.dat);
+
+fir_num_coefs=10;
+n_input_chans = size(XXX{end}.dat.(fns{1}).(signal), 3);
+append_module(MODULES.normalize_channels.mdl(struct('force_positive', true)));
+meanresp = nanmean(flatten_field(XXX{end}.dat,XXX{end}.training_set,'respavg'));
+append_module(MODULES.fir_filter.mdl(struct('num_coefs', fir_num_coefs, ...
+                                'baseline',meanresp,...
+                                'fit_fields', {{'coefs','baseline'}})));
+fitSubstacklsq();
