4834383a3a7c958936c14894a840f998be982ae8
jean.lienard <jean.lienard@email.wsu.edu>
2014-03-18 14:47:46 -0700

    new version of Lindeberg's time-causal kernels

---
+++ keywords/lindeberg11.m
@@ -0,0 +1,24 @@
+function lindeberg11()
+% March 2014 - lienard
+% modified from 'fir.m'
+
+% Linderberg's time-causal kernels of order (dx=1,dt=1)
+
+global MODULES XXX;
+
+dep_tau_norm=100;
+fir_num_coefs=20; % was 12 previously (JL)
+stop_exp=2.0;
+
+
+append_module(MODULES.normalize_channels.mdl(struct('force_positive', true)));
+
+meanresp = nanmean(flatten_field(XXX{end}.dat,XXX{end}.training_set,'respavg'));
+append_module(MODULES.lindeberg_filter.mdl(struct('num_coefs', fir_num_coefs, ...
+                                'order_x',1,...
+                                'order_t',1,...
+                                'baseline',meanresp,...
+                                'fit_fields', {{'lincoefs','baseline'}})));
+%                                 'fit_fields', {{'lincoefs'}})));
+
+fitSubstack([],10^-(stop_exp));
