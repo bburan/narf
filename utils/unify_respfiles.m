ec3965d334cdeb72c6065072ba25de147b559ffb
Stephen David <davids@ohsu.edu>
2014-03-26 14:07:50 -0700

    dbtickqueue added to fit_iteratively

--- utils/unify_respfiles.m
+++ utils/unify_respfiles.m
@@ -8,6 +8,8 @@ for ii = 2:length(xxxs)
         f = fns{jj};
         xxx.dat.(f) = xxxs{ii}.dat.(f);
     end
+    % union sorts by alphabetical order and can cause mismatch in order of
+    % file names and filecodes:
     %xxx.training_set = union(xxx.training_set, xxxs{ii}.training_set);
     %xxx.test_set = union(xxx.test_set, xxxs{ii}.test_set);
     %xxx.filecodes = union(xxx.filecodes, xxxs{ii}.filecodes);
