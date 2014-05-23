46cdd46a87fa38e21fbf4897086a73980b61080a
Stephen David <davids@ohsu.edu>
2014-05-23 13:49:42 -0700

    Help for load_phi.

--- utils/load_phi.m
+++ utils/load_phi.m
@@ -1,3 +1,31 @@
+% function [phi,phi_label,narfdata]=load_phi(cellid,batch,modelname);
+%
+% load all parameters that were fit for a model or set of models
+% matching the (cellid,batch,modelname) search criteria in the
+% NarfResults table.  
+%
+% inputs:
+% cellid - baphy cellid (or search string)
+% batch - baphy/narf data set number
+% modelname - narf model name
+%
+% outputs:
+% phi - a [parameter count X cell] matrix of fit parameter values,
+%       where each column contains all parameters for one model fit
+% phi_label - {parameter count X cell} cell array with a string
+%             identifying the model and parameter number of each
+%             value in phi.
+% narfdata - [cell X 1] structure array with metadata for each model
+%
+% "%" is a wildcared, so this sequence of commands will load fit
+% parameters for a bunch of cells fit with the same model: 
+% >> modelname='env100_logfree_dep1pc_fir15_siglog100_fit05';
+% >> batch=259;
+% >> cellid='%';
+% >> [phi,phi_label,narfdata]=load_phi(cellid,batch,modelname);
+%
+% created SVD 2014_05_23
+%
 function [phi,phi_label,narfdata]=load_phi(cellid,batch,modelname);
 
 global STACK
