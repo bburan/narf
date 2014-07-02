aa326d4b413b2ac33fc47e0ea14f5cc240ac8b16
Stephen David <davids@ohsu.edu>
2014-07-01 17:18:47 -0700

    nl_dummy

---
+++ keywords/fit05f.m
@@ -0,0 +1,98 @@
+% function fit05f()
+%
+% like fit05 but "fast"-- 
+% MaxStepsPerIteration=8
+% StepGrowth=1.2
+%
+function fit05f()
+
+nmse();
+
+MaxStepsPerIteration=8;
+StepGrowth=1.2;
+
+function [a,b,c,d] = step_until_10neg3(prev_opts)              
+    if exist('prev_opts', 'var')
+        [a,b,c,d] = fit_boo(prev_opts);
+    else
+        [a,b,c,d] = fit_boo('StopAtAbsScoreDelta', 10^-3, ...
+                            'StopAtStepSize', 10^-6,...
+                            'StopAtStepNumber', MaxStepsPerIteration, ...
+                            'StepGrowth', StepGrowth);
+    end
+end
+
+function [a,b,c,d] = step_until_10neg35(prev_opts)
+    if exist('prev_opts', 'var')
+        [a,b,c,d] = fit_boo(prev_opts);
+    else
+        [a,b,c,d] = fit_boo('StopAtAbsScoreDelta', 10^-3.5, ...
+                            'StopAtStepSize', 10^-6,...
+                            'StopAtStepNumber', MaxStepsPerIteration, ...
+                            'StepGrowth', StepGrowth);
+    end
+end
+
+function [a,b,c,d] = step_until_10neg4(prev_opts)
+    if exist('prev_opts', 'var')
+        [a,b,c,d] = fit_boo(prev_opts);
+    else
+        [a,b,c,d] = fit_boo('StopAtAbsScoreDelta', 10^-4, ...
+                            'StopAtStepSize', 10^-7,...
+                            'StopAtStepNumber', MaxStepsPerIteration, ...
+                            'StepGrowth', StepGrowth);
+    end
+end
+
+function [a,b,c,d] = step_until_10neg45(prev_opts)
+    if exist('prev_opts', 'var')
+        [a,b,c,d] = fit_boo(prev_opts);
+    else
+        [a,b,c,d] = fit_boo('StopAtAbsScoreDelta', 10^-4.5, ...
+                            'StopAtStepSize', 10^-7,...
+                            'StopAtStepNumber', MaxStepsPerIteration, ...
+                            'StepGrowth', StepGrowth);
+    end
+end
+
+function [a,b,c,d] = step_until_10neg5(prev_opts)
+    if exist('prev_opts', 'var')
+        [a,b,c,d] = fit_boo(prev_opts);
+    else
+        [a,b,c,d] = fit_boo('StopAtAbsScoreDelta', 10^-5, ...
+                            'StopAtStepNumber', MaxStepsPerIteration, ...
+                            'StepGrowth', StepGrowth);
+    end
+end
+
+function [a,b,c,d] = step_until_10neg6(prev_opts)
+    if exist('prev_opts', 'var')
+        [a,b,c,d] = fit_boo(prev_opts);
+    else
+        [a,b,c,d] = fit_boo('StopAtAbsScoreDelta', 10^-6, ...
+                            'StopAtStepNumber', MaxStepsPerIteration, ...
+                            'StepGrowth', StepGrowth);
+    end
+end
+
+fit_boo('StopAtAbsScoreDelta', 10^-2.0, ...
+        'StepGrowth', StepGrowth);
+
+fit_iteratively(@step_until_10neg3, ...
+                create_term_fn());
+
+fit_iteratively(@step_until_10neg35, ...
+                create_term_fn());
+
+fit_iteratively(@step_until_10neg4, ...
+                create_term_fn());
+
+fit_iteratively(@step_until_10neg45, ...
+                create_term_fn());
+
+fit_iteratively(@step_until_10neg5, ...
+                create_term_fn());
+
+%fit_iteratively(@step_until_10neg6, ...
+%                create_term_fn());
+end
---
+++ utils/nl_dummy.m
@@ -0,0 +1,8 @@
+%function ret = nl_dummy(phi, z)
+%
+%pass-through for temporary fits with nl disabled
+%
+function ret = nl_dummy(phi, z)
+
+    ret=z;
+end
