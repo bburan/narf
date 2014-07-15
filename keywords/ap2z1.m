27c1101ad1d1daf580d64b13fab9a71fd5dd6a7a
Ivar Thorson <thorsoni@ohsu.edu>
2014-07-15 16:49:37 -0700

    Adding new "advanced pz" keywords.

---
+++ keywords/ap2z1.m
@@ -0,0 +1,22 @@
+function ap2z1 ()
+
+global MODULES XXX STACK;
+
+% Init the coefs to have the right dimensionality for 'stim'
+x = XXX{end};
+fns = fieldnames(x.dat);
+sf = fns{1};
+[T, S, num_chans] = size(x.dat.(sf).stim);                  
+
+append_module(MODULES.normalize_channels.mdl(struct('force_positive', true)));
+
+append_module(MODULES.pole_zeros.mdl(...
+                struct('fit_fields', {{'poles', 'zeros', 'delays'}}, ...
+                       'n_poles', 2, ...
+                       'n_zeros', 1, ...                       
+                       'n_inputs', num_chans)));
+
+append_module(MODULES.normalize_channels);
+
+
+end
