f3400c6834a2c6ba92fa17029bbfec8d89142bcc
Ivar Thorson <thorsoni@ohsu.edu>
2014-07-17 14:48:15 -0700

    Adding dexp versions with only 3 parameters.

---
+++ keywords/dexp3d.m
@@ -0,0 +1,23 @@
+function dexp3d()
+
+global MODULES XXX;
+
+ff=XXX{end}.training_set{1};
+meanresp=nanmean(XXX{end}.dat.(ff).respavg(:));
+meanpred=nanmean(XXX{end}.dat.(ff).stim(:));
+resprange = max(XXX{end}.dat.(ff).respavg(:))-min(XXX{end}.dat.(ff).respavg(:));
+curvature = 1 / resprange;
+
+nl_dexp3 = @(p,z) nl_dexp([p(1) p(2) p(3) 1.2], z);
+
+append_module(MODULES.nonlinearity.mdl(...
+    struct('fit_fields', {{'phi'}}, ...
+           'phi', [0 meanresp*2 meanpred], ...
+           'nlfn', nl_dexp3)));
+
+% If STIM has the right number of channels, we
+% can fit it. If not, don't fit it. 
+if size(XXX{end}.dat.(ff).stim, 3) == 1
+    fitSubstack();
+end
+
